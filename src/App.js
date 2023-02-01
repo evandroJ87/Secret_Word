21f9ed916e207f91ae04b73dacb74ad7f176758b
Evandro <evandrosilva8744@gmail.com>
2023-02-01 13:52:09 -0300

    Adição de componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import StartScreen from './components/StartScreen';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Secret word</h1>
+    <div className="App"> 
+     < StartScreen />
     </div>
   );
 }
---
+++ src/components/StartScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const StartScreen = () => {
+  return (
+    <div className='start' >
+        <h1>Secret Word</h1>
+        <p>Clique aqui para iniciar:</p>
+        <button>Começar o jogo</button>
+    </div>
+  )
+}
+
+export default StartScreen
