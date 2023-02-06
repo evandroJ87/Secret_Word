5d6c0d9247552558f7f9cb655d3e93ee2397548a
Evandro <evandrosilva8744@gmail.com>
2023-02-06 00:23:17 -0300

    att

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 // React
 import { useCallback, useEffect, useState } from 'react';
 // data
-import {wordsList} from "./data/words"
+import {wordsList} from "./data/words";
+// Components
 import StartScreen from './components/StartScreen';
+import GameOver from './components/GameOver';
+import Game from './components/Game'
 
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+]
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+  console.log(words)
   return (
     <div className="App"> 
-     < StartScreen />
+     {gameStage==="start" && < StartScreen />}
+     {gameStage==="game" && < Game />}
+     {gameStage==="end" && < GameOver />}
     </div>
   );
 }
